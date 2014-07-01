c41710e7bd1dfaa08c3c07ace6c69e65fc9efdba
Shelley Hope <shelleyjhope@gmail.com>
2014-07-01 13:56:31 +0100

    first 'real' failing test'

--- Tests/WrapperTest.php
+++ Tests/WrapperTest.php
@@ -3,8 +3,9 @@
 require_once dirname(__File__) . '/../Wrapper.php';
 
 class WrapperTest extends PHPUnit_Framework_TestCase {  
-  function testCanCreateAWrapper() {
+  function testItShouldWrapAnEmptyString() {
     $wrapper = new Wrapper();
+    $this->assertEquals('', $wrapper->wrap(''));
   }
 }
 

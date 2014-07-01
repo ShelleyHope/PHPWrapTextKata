96738e0c235b9ded70a64a01f00411d9c0adccfa
Shelley Hope <shelleyjhope@gmail.com>
2014-07-01 15:01:15 +0100

    test for wrapping with space not at line end passes too

--- Wrapper.php
+++ Wrapper.php
@@ -3,12 +3,14 @@
 class Wrapper {
 
   function wrap($text, $lineLength) {
-    if (substr($text, $lineLength - 1, 1) == ' ')
-        return substr ($text, 0, strpos($text, ' ')) . "\n" . $this->wrap(substr($text, strpos($text, ' ') + 1), $lineLength);
-    if (strlen($text) > $lineLength)
-        return substr ($text, 0, $lineLength) . "\n" . $this->wrap(substr($text, $lineLength), $lineLength);
+    if (strlen($text) > $lineLength) {
+      if (strpos(substr($text, 0, $lineLength), ' ') !=0)
+        return substr($text, 0, strpos($text, ' ')) . "\n" . $this->wrap(substr($text, strpos($text, ' ') +1 ), $lineLength);
+      return substr($text, 0, $lineLength) . "\n" . $this->wrap(substr($text, $lineLength), $lineLength);
+    }
     return $text;
   }
+  
 }
 
 ?>

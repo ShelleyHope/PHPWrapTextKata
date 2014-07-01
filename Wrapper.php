f9cbfc7a12f6b419c8fe4fdb294795a21374625d
Shelley Hope <shelleyjhope@gmail.com>
2014-07-01 15:03:01 +0100

    refactored code

--- Wrapper.php
+++ Wrapper.php
@@ -2,15 +2,14 @@
 
 class Wrapper {
 
-  function wrap($text, $lineLength) {
-    if (strlen($text) > $lineLength) {
-      if (strpos(substr($text, 0, $lineLength), ' ') !=0)
-        return substr($text, 0, strpos($text, ' ')) . "\n" . $this->wrap(substr($text, strpos($text, ' ') +1 ), $lineLength);
-      return substr($text, 0, $lineLength) . "\n" . $this->wrap(substr($text, $lineLength), $lineLength);
-    }
-    return $text;
+ function wrap($text, $lineLength) {
+    if (strlen($text) <= $lineLength)
+        return $text;
+    if (strpos(substr($text, 0, $lineLength), ' ') != 0)
+        return substr ($text, 0, strpos($text, ' ')) . "\n" . $this->wrap(substr($text, strpos($text, ' ') + 1), $lineLength);
+    return substr ($text, 0, $lineLength) . "\n" . $this->wrap(substr($text, $lineLength), $lineLength);
   }
-  
+
 }
 
 ?>

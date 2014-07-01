9033276a1bfffa4a426ad6bd920e47cc36ecb8d6
Shelley Hope <shelleyjhope@gmail.com>
2014-07-01 14:28:51 +0100

    test for repeated wrapping now passes

--- Wrapper.php
+++ Wrapper.php
@@ -3,7 +3,7 @@
 class Wrapper {
   function wrap($text, $lineLength) {
     if (strlen($text) > $lineLength)
-      return substr($text, 0, $lineLength) . "\n" . substr($text, $lineLength);
+      return substr($text, 0, $lineLength) . "\n" . $this->wrap(substr($text, $lineLength), $lineLength);
     return $text;
   }
 }

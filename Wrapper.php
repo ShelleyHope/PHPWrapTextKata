b6cffeb5138308d4f720df0c1e6d5c7e409312de
Shelley Hope <shelleyjhope@gmail.com>
2014-07-01 14:41:38 +0100

    test for wrapping two words with a space at the end of the line now passes

--- Wrapper.php
+++ Wrapper.php
@@ -1,9 +1,12 @@
 <?php
 
 class Wrapper {
+
   function wrap($text, $lineLength) {
+    if (substr($text, $lineLength - 1, 1) == ' ')
+        return substr ($text, 0, strpos($text, ' ')) . "\n" . $this->wrap(substr($text, strpos($text, ' ') + 1), $lineLength);
     if (strlen($text) > $lineLength)
-      return substr($text, 0, $lineLength) . "\n" . $this->wrap(substr($text, $lineLength), $lineLength);
+        return substr ($text, 0, $lineLength) . "\n" . $this->wrap(substr($text, $lineLength), $lineLength);
     return $text;
   }
 }
